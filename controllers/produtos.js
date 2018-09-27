f7d4c221037122e99576bfacc7b6c28584a0dc3e
Artur Diniz Adam <artur.daz@gmail.com>
2018-09-27 11:02:26 -0300

    dados do mysql

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -1,19 +1,27 @@
-function lista(req, resp){
-    const produtos = [
-        {
-            titulo: "Titulo 1"
-        },
-        {
-            titulo: "Titulo 2"
-        },
-        {
-            titulo: "Titulo 3"
-        },
-    ]
+const mysql = require('mysql')
 
-    resp.render('produtos/lista', {
-        livros: produtos
+function lista(req, resp){
+
+    // conexao é push
+    const conexao = mysql.createConnection({
+        user: 'root',
+        password: '',
+        host: 'localhost',
+        database: 'cdc',
+        port: 32777
     })
+
+    // produtos é pull
+    conexao.query("SELECT * from livros", function(erro, produtos){
+        if(!erro){
+            resp.render('produtos/lista', {
+                livros: produtos
+            })
+        } else {
+            resp.send(erro)
+        }
+    })
+    
 }
 
 function cadastro(){
